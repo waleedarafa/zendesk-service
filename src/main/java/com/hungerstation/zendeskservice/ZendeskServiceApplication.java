e15051e8a5325299eb163237f7cfc3e1509520ea
Waleed Arafa <waleed.arafa@mondiamedia.com>
2018-08-12 14:53:44 +0200

    first commit

---
+++ src/main/java/com/hungerstation/zendeskservice/ZendeskServiceApplication.java
@@ -0,0 +1,12 @@
+package com.hungerstation.zendeskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZendeskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZendeskServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hungerstation/zendeskservice/ZendeskServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hungerstation.zendeskservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZendeskServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
